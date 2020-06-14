8670c0174fc88d31f3724b0fea688f22284f2bd4
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-06-14 15:18:20 -0300

    criação dos componentes: Footer, Header, NavBar

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () =>{
+  return (
+    <div className='py-4 text-center bg-gray-400'>
+    MyDailyStatus é um projeto criado durante o Fullstack. <br/>
+    Implementado por: Wanderson
+    </div>
+  )
+}
+
+export default Footer;
