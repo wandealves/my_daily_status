a5e23b68e43a810ff9a3f325c5bf51ed1eb5fe52
Wanderson <wanderson.alves.rodrigues@gmail.com>
2020-06-14 14:33:37 -0300

    inicialização projeto

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+    <h1><img src='/logo.png' alt='Olá' height='60'></img></h1>
+    </div>
+    )
+}
+
+export default Index;
